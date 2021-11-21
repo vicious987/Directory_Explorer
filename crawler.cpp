177249bb96db7cf3722ed88c585f4a3fbc611527
vicious <vicious987@gmail.com>
2021-11-21 21:14:28 +0100

    refactor dir crawler

--- crawler.cpp
+++ crawler.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdio>
 #include <iostream>
+#include <deque>
 
 
 //TODO: change counters from int to usigned longs
@@ -19,26 +20,33 @@ std::optional<int> count_lines(const std::filesystem::path &path){
     return lines;
 }
 
+int count_lines_in_files(const std::deque<std::filesystem::path> &paths){
+    int total = 0;
+    for (const auto & p: paths) {
+        auto cl = count_lines(p);
+        total += cl.value_or(0);
+    }
+    return total;
+}
+
 std::optional<dir_stats> directory_crawl(const std::filesystem::path &start_path, bool verbose) {
     if (!std::filesystem::is_directory(start_path)){
         return std::nullopt;
     }
     dir_stats res;
+    std::deque<std::filesystem::path> filepaths;
+
     for(auto const &d: std::filesystem::recursive_directory_iterator{start_path}){
         res.total_entries++;
         if (d.is_directory())
             res.dircount++;
         else {
+            filepaths.push_back(d);
             res.filecount++;
-            auto cl = count_lines(d.path());
-            if (cl.has_value()){
-                res.linecount += cl.value();
-            } else {
-                res.unreadable++;
-            }
         }
         //std::cout << d << "\n";
     }
+    res.linecount = count_lines_in_files(filepaths);
     if (verbose) {
         printf("total entries: %d, directory count: %d, file count: %d, total line count: %d \n",
             res.total_entries, res.dircount, res.filecount, res.linecount); // probably replace it with cout
